79d150bf1b19967ef84d97444e060681f8d6b501
zhaomsdemo <zhaomsdemo@gmail.com>
2020-07-28 17:30:43 -0700

    init

---
+++ src/main/java/com/zjh/pspet/PsPetApplication.java
@@ -0,0 +1,12 @@
+package com.zjh.pspet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PsPetApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(PsPetApplication.class, args);
+  }
+}
---
+++ src/test/java/com/zjh/pspet/PsPetApplicationTests.java
@@ -0,0 +1,11 @@
+package com.zjh.pspet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PsPetApplicationTests {
+
+  @Test
+  void contextLoads() {}
+}
