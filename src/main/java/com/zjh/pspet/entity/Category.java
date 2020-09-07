44701204ab01335e4762fb1eee59ad4bcd54262e
zhaomsdemo <zhaomsdemo@gmail.com>
2020-09-07 16:54:16 -0700

    Use spotlessApply before build

--- src/main/java/com/zjh/pspet/entity/Category.java
+++ src/main/java/com/zjh/pspet/entity/Category.java
@@ -3,9 +3,11 @@ package com.zjh.pspet.entity;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 import lombok.Data;
+import lombok.EqualsAndHashCode;
 
 @Document
 @Data
+@EqualsAndHashCode(callSuper = false)
 public class Category extends BaseEntity {
 
 	private String name;
--- src/main/java/com/zjh/pspet/entity/Tag.java
+++ src/main/java/com/zjh/pspet/entity/Tag.java
@@ -2,9 +2,11 @@ package com.zjh.pspet.entity;
 
 import lombok.Builder;
 import lombok.Data;
+import lombok.EqualsAndHashCode;
 
 @Builder
 @Data
+@EqualsAndHashCode(callSuper = false)
 public class Tag extends BaseEntity {
 
 	private String name;
